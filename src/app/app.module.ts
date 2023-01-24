b8ed94d7328b3a434d9063c01d762d701dbf85a9
Miguel Angel Hernandez Perdomo <migle.hp@gmail.com>
2023-01-24 18:42:01 +0100

    repositorio creado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
+import { FavComponent } from './fav/fav.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SesionComponent } from './sesion/sesion.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent,
+    FavComponent,
+    NavbarComponent,
+    SesionComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/sesion/sesion.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SesionComponent } from './sesion.component';
+
+describe('SesionComponent', () => {
+  let component: SesionComponent;
+  let fixture: ComponentFixture<SesionComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SesionComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SesionComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
